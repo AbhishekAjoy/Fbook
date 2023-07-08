980f9317da08b71a763696439409ff921d16cc8a
AbhishekAjoy <abhiajoy23@gmail.com>
2023-07-08 14:14:23 +0530

    Home Page and User profile created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { RegisterComponent } from './components/register/register.component'
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { UserProfileComponent } from './components/shared/user-profile/user-profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { NetworkComponent } from './components/network/network.component';
+import { FriendsComponent } from './components/friends/friends.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
     RegisterComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    UserProfileComponent,
+    HomeComponent,
+    NetworkComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
